614aaf48dfbf014cbae1e2aefa279f07096eee4a
Quillosa <denden171.quillosa@gmail.comgit condig --global user.name Quillosagit config --global user.email denden171.quillosa@gmail.com>
2024-11-13 11:21:16 +0800

    just a push

---
+++ developers.php
@@ -0,0 +1,66 @@
+<?php include('header_dashboard.php'); ?>
+    <body id="class_div">
+		<?php include('navbar_about.php'); ?>
+        <div class="container-fluid">
+            <div class="row-fluid">
+                <div class="span12" id="content">
+                     <div class="row-fluid">
+                        <!-- block -->
+                        <div class="block">
+								<div class="navbar navbar-inner block-header">
+									<div id="" class="muted pull-right"><a href="index.php"><i class="icon-arrow-left"></i> Back</a></div>
+								</div>
+                            <div class="block-content collapse in">
+							<h3>Developers</h3>
+							<hr>
+                                <div class="span3">
+										<center>
+										<img id="developers" src="admin/images/dpdiscord.jpg" class="img-circle">
+										<hr>
+										<p>Name: Daniel Rasheed Quillosa</p>
+										<p>Address: Bulacan City</p>
+										<p>Email: Daga@gmail.com</p>
+										<p>Position: Programmer</p>
+										</center>
+								</div>
+                                <div class="span3">
+															<center>
+								<img id="developers" src="admin/images/picturecarats.jpg" class="img-circle">
+								<hr>
+										<p>Name: Joshua Carats</p>
+										<p>Address: STI City</p>
+										<p>Email: pinat@gmail.com</p>
+										<p>Position: Programmer</p>
+								</center>
+								</div>
+                                <div class="span3">
+															<center>
+								<img id="developers" src="admin/images/2x2.jpg" class="img-circle">
+								<hr>
+										<p>Name: Denden</p>
+										<p>Address: Ate neah</p>
+										<p>Email: haha@gmail.com</p>
+										<p>Position: Project Manager</p>
+								</center>
+								</div>
+                                <div class="span3">
+															<center>
+								<img id="developers" src="admin/images/dpdiscord.jpg" class="img-circle">
+								<hr>
+										<p>Name: Rio Jean Mauleon</p>
+										<p>Address: Walter</p>
+										<p>Email: rio@gmail.com</p>
+										<p>Position: Project Manager</p>
+								</center>
+								</div>
+                            </div>
+                        </div>
+                        <!-- /block -->
+                    </div>
+                </div>
+            </div>
+		<?php include('footer.php'); ?>
+        </div>
+		<?php include('script.php'); ?>
+    </body>
+</html>
---
+++ title_index.php
@@ -0,0 +1,38 @@
+				
+								<div class="row-fluid">
+
+						<div class="span12">
+						
+						</div>	
+													
+							</div>
+							<div class="row-fluid">
+
+						<div class="span4">
+						<img class="index_logo" src="admin/images/nobg.png">
+						</div>	
+						<div class="span8">
+						
+								<div class="title">
+							<p class="chmsc">Breakthrough Christian Fellowship Church</p>
+							<h3>
+
+							<p>Breakthrough System</p>
+						
+							</h3>		
+						</div>
+			
+						</div>							
+							</div>
+				
+				<div class="row-fluid">
+
+						<div class="span12">
+						<br>
+								<div class="motto">
+												<p>Welcome to the BCF: Breakthrough!</p>
+												<p>We are delighted to have you here. Empowering Leaders, Discipling Nations.</p>
+												<p>"Together, we grow in faith and love."</p>
+								</div>		
+						</div>		
+				</div>
