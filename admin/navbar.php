daf3effb42d0f639c9f46ba7825a7bb807778796
Quillosa <denden171.quillosa@gmail.comgit condig --global user.name Quillosagit config --global user.email denden171.quillosa@gmail.com>
2024-11-25 03:14:50 +0800

    error nav fix

--- admin/navbar.php
+++ admin/navbar.php
@@ -1,33 +1,92 @@
-  <div class="navbar navbar-fixed-top navbar-inverse">
-            <div class="navbar-inner">
-                <div class="container-fluid">
-                    <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span>
-                     <span class="icon-bar"></span>
-                     <span class="icon-bar"></span>
-                    </a>
-                    <span class="brand" href="#">Breakthrough ADMIN Panel</span>
-                    <div class="nav-collapse collapse">
-                        <ul class="nav pull-right">
-						<?php $query= mysqli_query($conn,"select * from users where user_id = '$session_id'")or die(mysqli_error());
-								$row = mysqli_fetch_array($query);
-						?>
-                            <li class="dropdown">
-                                <a href="#" role="button" class="dropdown-toggle" data-toggle="dropdown"> <i class="icon-user icon-large"></i><?php echo $row['firstname']." ".$row['lastname'];  ?> <i class="caret"></i>
-
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a tabindex="-1" href="#">Profile</a>
-                                    </li>
-                                    <li class="divider"></li>
-                                    <li>
-                                        <a tabindex="-1" href="logout.php"><i class="icon-signout"></i>&nbsp;Logout</a>
-                                    </li>
-                                </ul>
+<div class="navbar navbar-fixed-top navbar-inverse">
+    <div class="navbar-inner">
+        <div class="container-fluid">
+            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </a>
+            <span class="brand" href="#">Breakthrough ADMIN Panel</span>
+            <div class="nav-collapse collapse">
+                <ul class="nav pull-right">
+                    <?php
+                    if (isset($session_id)) {
+                        $query = mysqli_query($conn, "SELECT * FROM users WHERE user_id = '$session_id'") or die(mysqli_error($conn));
+                        if ($row = mysqli_fetch_array($query)) {
+                            $firstname = $row['firstname'];
+                            $lastname = $row['lastname'];
+                        } else {
+                            $firstname = "Guest";
+                            $lastname = "";
+                        }
+                    } else {
+                        $firstname = "Guest";
+                        $lastname = "";
+                    }
+                    ?>
+                    <li class="dropdown">
+                        <a href="#" role="button" class="dropdown-toggle" data-toggle="dropdown">
+                            <i class="icon-user icon-large"></i>
+                            <?php echo $firstname . " " . $lastname; ?>
+                            <i class="caret"></i>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a tabindex="-1" href="#">Profile</a>
+                            </li>
+                            <li class="divider"></li>
+                            <li>
+                                <a tabindex="-1" href="logout.php"><i class="icon-signout"></i>&nbsp;Logout</a>
                             </li>
                         </ul>
-                    </div>
-                    <!--/.nav-collapse -->
-                </div>
+                    </li>
+                </ul>
             </div>
+            <!--/.nav-collapse -->
         </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+<div class="navbar navbar-fixed-top navbar-inverse">
+    <div class="navbar-inner">
+        <div class="container-fluid">
+            <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </a>
+            <span class="brand" href="#">Breakthrough ADMIN Panel</span>
+            <div class="nav-collapse collapse">
+                <ul class="nav pull-right">
+                    <?php $query= mysqli_query($conn,"select * from users where user_id = '$session_id'")or die(mysqli_error());
+                    $row = mysqli_fetch_array($query);
+                    ?>
+                    <li class="dropdown">
+                        <a href="#" role="button" class="dropdown-toggle" data-toggle="dropdown"> <i class="icon-user icon-large"></i><?php echo $row['firstname']." ".$row['lastname'];  ?> <i class="caret"></i>
+
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a tabindex="-1" href="#">Profile</a>
+                            </li>
+                            <li class="divider"></li>
+                            <li>
+                                <a tabindex="-1" href="logout.php"><i class="icon-signout"></i>&nbsp;Logout</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <!--/.nav-collapse -->
+        </div>
+    </div>
+</div>
